4d2737713f6fd72b843dfec3f7e4aa920f5c2589
nestamsgossip-wq <nestamsgossip@gmail.com>
2026-07-22 19:31:05 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-AzjlOzmdQblL233hZQwi0Ege100FqyE",
+  authDomain: "blessed-divine-prasadam-d6699.firebaseapp.com",
+  projectId: "blessed-divine-prasadam-d6699",
+  storageBucket: "blessed-divine-prasadam-d6699.firebasestorage.app",
+  messagingSenderId: "610812667945",
+  appId: "1:610812667945:web:e795af1fcb6ac2706ad438"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
