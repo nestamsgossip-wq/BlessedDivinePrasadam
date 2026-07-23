38abb05d6816c76df02126602daf15a57cd0940c
nestamsgossip-wq <nestamsgossip@gmail.com>
2026-07-23 05:56:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Blessed Divine Prasadam loaded successfully");
+document.addEventListener("DOMContentLoaded", () => {
+    const signupBtn = document.getElementById("signupBtn");
+    const loginBtn = document.getElementById("loginBtn");
+
+    if (signupBtn) {
+        signupBtn.addEventListener("click", () => {
+            alert("Signup button clicked!");
+        });
+    }
+
+    if (loginBtn) {
+        loginBtn.addEventListener("click", () => {
+            alert("Login button clicked!");
+        });
+    }
+});
